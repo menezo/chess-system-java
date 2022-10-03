af2610b96c1b0de16d805e40ace84f96ab0bccf2
Joao Menezes <joaovitormenezesm12@gmail.com>
2022-10-03 11:03:34 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -22,6 +22,10 @@ public abstract class ChessPiece extends Piece{
 		return moveCount;
 	}
 	
+	public ChessPosition getChessPosition() {
+	 return ChessPosition.fromPosition(position);
+	}
+	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
